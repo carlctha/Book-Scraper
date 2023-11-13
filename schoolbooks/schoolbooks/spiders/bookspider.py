c517887bdc8b02bed1af5727b0cae8359f6219f0
Carl <carlctha1@gmail.com>
2023-11-14 00:24:13 +0100

    Added feature extraction of book name and link

--- schoolbooks/schoolbooks/spiders/bookspider.py
+++ schoolbooks/schoolbooks/spiders/bookspider.py
@@ -7,4 +7,11 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://campusbokhandeln.se/kurser/linkopings-universitet/data-it"]
 
     def parse(self, response):
-        pass
+        books = response.css("table#gridtable tbody tr")
+        for book in books:
+            book_name = book.css("a.tw-break-all::text").extract()
+            book_page_link = "https://campusbokhandeln.se/kurser/linkopings-universitet/data-it" + book.css("a").attrib["href"]
+            yield {
+                "name": book_name[0],
+                "link": book_page_link
+            }
