9caf00c1b22b8cd0052493f9185470d85a17bb0b
Carl <carlctha1@gmail.com>
2023-11-14 23:06:39 +0100

    Implemented mail sending feature

--- schoolbooks/schoolbooks/spiders/bookspider.py
+++ schoolbooks/schoolbooks/spiders/bookspider.py
@@ -1,18 +1,31 @@
-import scrapy
+import scrapy, smtplib
+from scrapy.crawler import CrawlerProcess
+from email.message import EmailMessage
+from decouple import config
 
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
     allowed_domains = ["campusbokhandeln.se"]
     start_urls = [
-        "https://campusbokhandeln.se/kurser/linkopings-universitet/data-it"
+        "https://campusbokhandeln.se/kurser/linkopings-universitet/data-it",
+        "https://campusbokhandeln.se/kurser/linkopings-universitet/matematik-statistik"
     ]
+
+    custom_settings = {
+        "FEEDS": {
+            "books.csv": {
+                "format": "csv"
+            }
+        }
+    }
 
     def parse(self, response):
         books = response.css("table#gridtable tbody tr")
         my_courses = [
             "725G90", "TSEA28", "TDDD86", "TDDD37",
-            ]
+            "TATA24", "92MA14", "TATA83", "TNIU66",
+        ]
         
         for book in books:
             course_code = book.css("a::text").extract()[1]
@@ -36,3 +49,33 @@ class BookspiderSpider(scrapy.Spider):
                 "name": book_name.extract_first(),
                 "price": book_price.extract_first()
             }
+
+
+def send_mail():
+    EMAIL_USER = config("EMAIL_USER")
+    EMAIL_PASS = config("EMAIL_PASS")
+
+    with open("books.csv", "r") as file:
+        data = file.read()
+
+    message = EmailMessage()
+    message["From"] = EMAIL_USER
+    message["To"] = EMAIL_USER
+    message["subject"] = "Kursböcker"
+    message.set_content("message body")
+    message.add_attachment(data, filename="books.csv")
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.ehlo()
+    server.login(EMAIL_USER, EMAIL_PASS)
+    server.send_message(message)
+    server.quit()
+
+
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(BookspiderSpider)
+    process.start()
+
+    send_mail()
