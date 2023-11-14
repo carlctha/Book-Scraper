76766dfe5ab8c8b3be44f047f9b4116112a25aac
Carl <carlctha1@gmail.com>
2023-11-14 21:52:05 +0100

    Implemented extraction of price and name from the books own page

--- schoolbooks/schoolbooks/spiders/bookspider.py
+++ schoolbooks/schoolbooks/spiders/bookspider.py
@@ -4,14 +4,35 @@ import scrapy
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
     allowed_domains = ["campusbokhandeln.se"]
-    start_urls = ["https://campusbokhandeln.se/kurser/linkopings-universitet/data-it"]
+    start_urls = [
+        "https://campusbokhandeln.se/kurser/linkopings-universitet/data-it"
+    ]
 
     def parse(self, response):
         books = response.css("table#gridtable tbody tr")
+        my_courses = [
+            "725G90", "TSEA28", "TDDD86", "TDDD37",
+            ]
+        
         for book in books:
-            book_name = book.css("a.tw-break-all::text").extract()
-            book_page_link = "https://campusbokhandeln.se/kurser/linkopings-universitet/data-it" + book.css("a").attrib["href"]
+            course_code = book.css("a::text").extract()[1]
+            book_page_url = "https://campusbokhandeln.se" + book.css("a").attrib["href"]
+            if course_code in my_courses:
+                yield response.follow(book_page_url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        book_list = response.css("div#books div.product-module")
+        book_name = response.css("div#books p.title a::text")
+        book_price = response.css("div#books p.price span::text")
+
+        if len(book_list) > 1:
+            for i in range(len(book_list)):
+                yield {
+                    "name": book_name.extract()[i],
+                    "price": book_price.extract()[i]
+                }
+        else:
             yield {
-                "name": book_name[0],
-                "link": book_page_link
+                "name": book_name.extract_first(),
+                "price": book_price.extract_first()
             }
